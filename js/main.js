122482368bf539c79a66b51433651cdad6816010
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-02-07 12:00:47 -0300

    Cliente

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-'use strict'
+"use strict";
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+const openModal = () =>
+  document.getElementById("modal").classList.add("active");
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+const closeModal = () =>
+  document.getElementById("modal").classList.remove("active");
 
 const tempClient = {
-    nome: "Fernanda",
-    email: "farnandarocha@gmail.com",
-    celular: "11991992813"
-}
-
-
+  nome: "Fernanda",
+  email: "farnandarocha@gmail.com",
+  celular: "11991992813",
+  cidade: "Santos",
+};
 
 //--------------CRUD-------------------
 
 //Create
 
 const createClient = (client) => {
-    
+    const db_client = {}
+    db_client.push (client)
+  localStorage.setItem("db_client", JSON.stringify(db_client))
 }
 
 //Eventos
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
+document
+  .getElementById("cadastrarCliente")
+  .addEventListener("click", openModal);
 
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
+document.getElementById("modalClose").addEventListener("click", closeModal);
